f9661b0b154a85d472e572e7b5b962d53ada93a0
jojoe77777 <jojoefivesevens@gmail.com>
2017-09-18 13:16:44 +1000

    Updated for MCPE 1.2 (ALPHA8)

--- src/SlapperRotation/Main.php
+++ src/SlapperRotation/Main.php
@@ -12,66 +12,62 @@ use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase implements Listener {
 
-    public function onEnable(){
-        $this->saveDefaultConfig();
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    }
+	public function onEnable() {
+		$this->saveDefaultConfig();
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
 
-    public function onPlayerMove(PlayerMoveEvent $ev){
-        $player = $ev->getPlayer();
-        $from = $ev->getFrom();
-        $to = $ev->getTo();
-        if($from->distance($to) < 0.1){
-            return;
-        }
-        $maxDistance = $this->getConfig()->get("max-distance");
-        foreach($player->getLevel()->getNearbyEntities($player->getBoundingBox()->grow($maxDistance, $maxDistance, $maxDistance), $player) as $e){
-            if($e instanceof Player){
-                continue;
-            }
-            if(substr($e->getSaveId(), 0, 7) !== "Slapper"){
-                continue;
-            }
-            switch($e->getSaveId()){
-                case "SlapperFallingSand":
-                case "SlapperMinecart":
-                case "SlapperBoat":
-                case "SlapperPrimedTNT":
-                case "SlapperShulker":
-                    continue 2;
-            }
-            $xdiff = $player->x - $e->x;
-            $zdiff = $player->z - $e->z;
-            $angle = atan2($zdiff, $xdiff);
-            $yaw = (($angle * 180) / M_PI) - 90;
-            $ydiff = $player->y - $e->y;
-            $v = new Vector2($e->x, $e->z);
-            $dist = $v->distance($player->x, $player->z);
-            $angle = atan2($dist, $ydiff);
-            $pitch = (($angle * 180) / M_PI) - 90;
+	public function onPlayerMove(PlayerMoveEvent $ev) {
+		$player = $ev->getPlayer();
+		$from = $ev->getFrom();
+		$to = $ev->getTo();
+		if($from->distance($to) < 0.1) {
+			return;
+		}
+		$maxDistance = $this->getConfig()->get("max-distance");
+		foreach ($player->getLevel()->getNearbyEntities($player->getBoundingBox()->grow($maxDistance, $maxDistance, $maxDistance), $player) as $e) {
+			if($e instanceof Player) {
+				continue;
+			}
+			if(substr($e->getSaveId(), 0, 7) !== "Slapper") {
+				continue;
+			}
+			switch ($e->getSaveId()) {
+				case "SlapperFallingSand":
+				case "SlapperMinecart":
+				case "SlapperBoat":
+				case "SlapperPrimedTNT":
+				case "SlapperShulker":
+					continue 2;
+			}
+			$xdiff = $player->x - $e->x;
+			$zdiff = $player->z - $e->z;
+			$angle = atan2($zdiff, $xdiff);
+			$yaw = (($angle * 180) / M_PI) - 90;
+			$ydiff = $player->y - $e->y;
+			$v = new Vector2($e->x, $e->z);
+			$dist = $v->distance($player->x, $player->z);
+			$angle = atan2($dist, $ydiff);
+			$pitch = (($angle * 180) / M_PI) - 90;
 
-            if($e->getSaveId() === "SlapperHuman"){
-                $pk = new MovePlayerPacket();
-                $pk->entityRuntimeId = $e->getId();
-                $pk->x = $e->x;
-                $pk->y = $e->y + $e->getEyeHeight();
-                $pk->z = $e->z;
-                $pk->yaw = $yaw;
-                $pk->pitch = $pitch;
-                $pk->bodyYaw = $yaw;
-                $pk->onGround = $e->onGround;
-            } else {
-                $pk = new MoveEntityPacket();
-                $pk->entityRuntimeId = $e->getId();
-                $pk->x = $e->x;
-                $pk->y = $e->y;
-                $pk->z = $e->z;
-                $pk->yaw = $yaw;
-                $pk->headYaw = $yaw;
-                $pk->pitch = $pitch;
-            }
-            $player->dataPacket($pk);
-        }
-    }
+			if($e->getSaveId() === "SlapperHuman") {
+				$pk = new MovePlayerPacket();
+				$pk->entityRuntimeId = $e->getId();
+				$pk->position = $e->asVector3()->add(0, $e->getEyeHeight(), 0);
+				$pk->yaw = $yaw;
+				$pk->pitch = $pitch;
+				$pk->bodyYaw = $yaw;
+				$pk->onGround = $e->onGround;
+			} else {
+				$pk = new MoveEntityPacket();
+				$pk->entityRuntimeId = $e->getId();
+				$pk->position = $e->asVector3();
+				$pk->yaw = $yaw;
+				$pk->headYaw = $yaw;
+				$pk->pitch = $pitch;
+			}
+			$player->dataPacket($pk);
+		}
+	}
 
 }
