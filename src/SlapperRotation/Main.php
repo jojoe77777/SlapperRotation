e9b160538a17216d20a2fbb4863b0cb79b89936d
iiFlamiinBlaze <iiflamiinblaze123@gmail.com>
2019-07-12 14:48:06 -0400

    Updated to 1.12 packet changes

--- src/SlapperRotation/Main.php
+++ src/SlapperRotation/Main.php
@@ -5,7 +5,7 @@ namespace SlapperRotation;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\math\Vector2;
-use pocketmine\network\mcpe\protocol\MoveEntityAbsolutePacket;
+use pocketmine\network\mcpe\protocol\MoveActorAbsolutePacket;
 use pocketmine\network\mcpe\protocol\MovePlayerPacket;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
@@ -59,7 +59,7 @@ class Main extends PluginBase implements Listener {
 				$pk->headYaw = $yaw;
 				$pk->onGround = $e->onGround;
 			} else {
-				$pk = new MoveEntityAbsolutePacket();
+				$pk = new MoveActorAbsolutePacket();
 				$pk->entityRuntimeId = $e->getId();
 				$pk->position = $e->asVector3();
 				$pk->xRot = $pitch;
